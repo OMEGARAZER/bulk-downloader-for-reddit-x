4d3f0f986279051ff10460936adfbb45a2d04624
Serene-Arc <serenical@gmail.com>
2021-10-02 13:43:15 +1000

    Add Youtube test case

--- tests/site_downloaders/test_youtube.py
+++ tests/site_downloaders/test_youtube.py
@@ -15,6 +15,7 @@ from bdfr.site_downloaders.youtube import Youtube
 @pytest.mark.parametrize(('test_url', 'expected_hash'), (
     ('https://www.youtube.com/watch?v=uSm2VDgRIUs', '2d60b54582df5b95ec72bb00b580d2ff'),
     ('https://www.youtube.com/watch?v=GcI7nxQj7HA', '5db0fc92a0a7fb9ac91e63505eea9cf0'),
+    ('https://youtu.be/TMqPOlp4tNo', 'f68c00b018162857f3df4844c45302e7'),  # Age restricted
 ))
 def test_find_resources_good(test_url: str, expected_hash: str):
     test_submission = MagicMock()
