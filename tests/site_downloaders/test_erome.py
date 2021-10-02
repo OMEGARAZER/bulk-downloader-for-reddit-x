c6c6002ab2bbacf4c3440c25b50440d3cda80d16
Serene-Arc <serenical@gmail.com>
2021-10-02 12:52:12 +1000

    Update Erome module

--- tests/site_downloaders/test_erome.py
+++ tests/site_downloaders/test_erome.py
@@ -30,25 +30,18 @@ def test_get_link(test_url: str, expected_urls: tuple[str]):
 
 @pytest.mark.online
 @pytest.mark.slow
-@pytest.mark.parametrize(('test_url', 'expected_hashes'), (
-    ('https://www.erome.com/a/vqtPuLXh', {
-        '5da2a8d60d87bed279431fdec8e7d72f'
-    }),
-    ('https://www.erome.com/a/lGrcFxmb', {
-        '0e98f9f527a911dcedde4f846bb5b69f',
-        '25696ae364750a5303fc7d7dc78b35c1',
-        '63775689f438bd393cde7db6d46187de',
-        'a1abf398cfd4ef9cfaf093ceb10c746a',
-        'bd9e1a4ea5ef0d6ba47fb90e337c2d14'
-    }),
+@pytest.mark.parametrize(('test_url', 'expected_hashes_len'), (
+    ('https://www.erome.com/a/vqtPuLXh', 1),
+    ('https://www.erome.com/a/4tP3KI6F', 1),
 ))
-def test_download_resource(test_url: str, expected_hashes: tuple[str]):
+def test_download_resource(test_url: str, expected_hashes_len: int):
     # Can't compare hashes for this test, Erome doesn't return the exact same file from request to request so the hash
     # will change back and forth randomly
     mock_submission = MagicMock()
     mock_submission.url = test_url
     test_site = Erome(mock_submission)
     resources = test_site.find_resources()
-    [res.download() for res in resources]
+    for res in resources:
+        res.download()
     resource_hashes = [res.hash.hexdigest() for res in resources]
-    assert len(resource_hashes) == len(expected_hashes)
+    assert len(resource_hashes) == expected_hashes_len
