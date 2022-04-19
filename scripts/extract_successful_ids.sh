eb8f9d5876437ce28be8be184895874ebef14e3c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-18 20:47:35 -0500

    oh there's another one

--- scripts/extract_successful_ids.sh
+++ scripts/extract_successful_ids.sh
@@ -7,17 +7,10 @@ else
     exit 1
 fi
 
-if [ -n "$2" ]; then
-    output="$2"
-    echo "Outputting IDs to $output"
-else
-    output="./successful.txt"
-fi
-
 {
     grep 'Downloaded submission' "$file" | awk '{ print $(NF-2) }' ;
     grep 'Resource hash' "$file" | awk '{ print $(NF-2) }' ;
     grep 'Download filter' "$file" | awk '{ print $(NF-3) }' ;
     grep 'already exists, continuing' "$file" | awk '{ print $(NF-3) }' ;
     grep 'Hard link made' "$file" | awk '{ print $(NF) }' ;
-} >> "$output"
+}
