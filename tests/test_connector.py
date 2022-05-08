bfd481739b28c132e6bcc35595e3853a30d51a53
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-05-08 08:45:34 -0500

    Update test_connector.py

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -448,6 +448,7 @@ def test_check_user_existence_banned(
 @pytest.mark.parametrize(('test_subreddit_name', 'expected_message'), (
     ('donaldtrump', 'cannot be found'),
     ('submitters', 'private and cannot be scraped')
+    ('lhnhfkuhwreolo', 'does not exist')
 ))
 def test_check_subreddit_status_bad(test_subreddit_name: str, expected_message: str, reddit_instance: praw.Reddit):
     test_subreddit = reddit_instance.subreddit(test_subreddit_name)
