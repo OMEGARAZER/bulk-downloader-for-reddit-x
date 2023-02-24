35ecc37afee638d7a24afa9be672b8e2e6e1a42a
OMEGARAZER <869111+OMEGARAZER@users.noreply.github.com>
2023-02-24 14:48:33 -0500

    Bandit fixes
    Fixes S101 in connector
    Noqa S104 in oauth2

--- bdfr/oauth2.py
+++ bdfr/oauth2.py
@@ -75,7 +75,7 @@ class OAuth2Authenticator:
     def receive_connection() -> socket.socket:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind(("0.0.0.0", 7634))
+        server.bind(("0.0.0.0", 7634))  # noqa: S104
         logger.log(9, "Server listening on 0.0.0.0:7634")
 
         server.listen(1)
