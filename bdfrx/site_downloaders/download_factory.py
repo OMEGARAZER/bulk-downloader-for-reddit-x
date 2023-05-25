d19e7e95cd531eadceeea488fe8dae033b977c51
OMEGARAZER <869111+OMEGARAZER@users.noreply.github.com>
2023-05-25 13:27:11 -0400

    Short circuit  empty urls
    Handles empty urls returned by the api faster in factory.
    
    Removes failing imgur test.
    Replaces failing erome test.

--- bdfrx/site_downloaders/download_factory.py
+++ bdfrx/site_downloaders/download_factory.py
@@ -23,6 +23,8 @@ from bdfrx.site_downloaders.youtube import Youtube
 class DownloadFactory:
     @staticmethod
     def pull_lever(url: str) -> type[BaseDownloader]:  # noqa: PLR0911,PLR0912
+        if not url:
+            raise NotADownloadableLinkError("No url provided by the reddit API")
         sanitised_url = DownloadFactory.sanitise_url(url).lower()
         if re.match(r"(i\.|m\.|o\.)?imgur", sanitised_url):
             return Imgur
--- tests/site_downloaders/test_imgur.py
+++ tests/site_downloaders/test_imgur.py
@@ -80,7 +80,6 @@ def test_get_id(test_url: str, expected: str):
         ("https://imgur.com/a/1qzfWtY/gifv", ("65fbc7ba5c3ed0e3af47c4feef4d3735",)),
         ("https://imgur.com/a/1qzfWtY/mp4", ("65fbc7ba5c3ed0e3af47c4feef4d3735",)),
         ("https://imgur.com/a/1qzfWtY/spqr", ("65fbc7ba5c3ed0e3af47c4feef4d3735",)),
-        ("https://i.imgur.com/expO7Rc.gifv", ("e309f98158fc98072eb2ae68f947f421",)),
         ("https://i.imgur.com/a/aqpiMuL.gif", ("5b2a9a5218bf43dc26ba41389410c981",)),
     ),
 )
