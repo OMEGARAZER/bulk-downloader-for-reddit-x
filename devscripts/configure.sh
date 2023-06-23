586cb01133102d34070df67567dd7c85a24bbeb5
OMEGARAZER <869111+OMEGARAZER@users.noreply.github.com>
2023-06-23 00:57:32 -0400

    Correct devscripts

--- devscripts/configure.sh
+++ devscripts/configure.sh
@@ -3,6 +3,6 @@
 if [ -n "$REDDIT_CLIENT" ] && [ -n "$REDDIT_TOKEN" ]
 then
     cp ./bdfrx/default_config.cfg ./tests/test_config.cfg
-    sed -i '/client_id = /Id' ./tests/test_config.cfg
+    sed -i"" -e "/client_id =/Id" ./tests/test_config.cfg
     echo -e "\nclient_id = $REDDIT_CLIENT\nuser_token = $REDDIT_TOKEN" >> ./tests/test_config.cfg
 fi
